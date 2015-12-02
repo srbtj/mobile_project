a928c6762570c7d57621125ae63cbafeeed20e27
srbtj <srbtj@aliyun.com>
2015-12-02 13:05:55 +0800

    welcome page

--- js/index.js
+++ js/index.js
@@ -1,7 +1,29 @@
 window.onload = function(){
 
-    var doc = document;
+    var
+        doc = document,
+        img_arr = ['image/1.jpg','image/2.jpg','image/3.jpg','image/4.jpg','image/5.jpg'],
+        count = 0;
 
     doc.body.style.height = getViewInfo().h + 'px';
+
+    loadImg();
+    function loadImg(){
+
+        for(var i= 0,iLen=img_arr.length;i<iLen;i++){
+
+            var oImg = new Image();
+            oImg.src = img_arr[i];
+
+            oImg.onload = function(){
+                count++;
+
+                if(count === iLen){
+
+                    alert('Í¼Æ¬¼ÓÔØÍê³É');
+                }
+            }
+        }
+    }
 
 }
